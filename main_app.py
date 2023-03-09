c8bf22dedc2ccdf10ea7da75edfbac57a73c3616
atuad7535 <95579439+atuad7535@users.noreply.github.com>
2023-03-09 11:00:22 +0530

    Add files via upload

--- main_app.py
+++ main_app.py
@@ -1,11 +1,6 @@
-from operator import index
+
 
 import streamlit as st
-
-import plotly.express as px
-from autonlp import autonlp
-from sklearn import *
-import pandas_profiling
 import pandas as pd
 from streamlit_pandas_profiling import st_profile_report
 import os
@@ -14,9 +9,6 @@ if os.path.exists('./dataset.csv'):
     df_csv = pd.read_csv('dataset.csv', index_col=None)
 
 
-@st.cache
-def main():
-    pass
 
 
 with st.sidebar:
